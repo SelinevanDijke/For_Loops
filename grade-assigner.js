88cd71604d3fa12e68ddf7bc07c4ecbdb7a98bb8
Seline <74591087+SelinevanDijke@users.noreply.github.com>
2021-07-16 12:46:39 +0200

    Add files via upload

--- grade-assigner.js
+++ grade-assigner.js
@@ -13,31 +13,6 @@ function assignGrade(score) {
 }
 
 // let score = 60;
-for (let score = 60; score <= 65; score++) {
-    assignGrade();
-    console.log("For scoring " + score + " points, you get an E");
+for (let i = 60; i <= 100; i++) {
+  console.log("For scoring " + i + " points, you get an " + assignGrade(i));
 }
-
-for (let score = 65; score <= 70; score++) {
-    assignGrade();
-    console.log("For scoring " + score + " points, you get an D");
-}
-
-for (let score = 70; score <= 80; score++) {
-    assignGrade();
-    console.log("For scoring " + score + " points, you get an C");
-}
-
-for (let score = 80; score <= 90; score++) {
-    assignGrade();
-    console.log("For scoring " + score + " points, you get an B");
-}
-
-
-for (let score = 90; score <= 100; score++) {
-    assignGrade();
-    console.log("For scoring " + score + " points, you get an A");
-}
-
-
-
--- multiplication-tables.js
+++ multiplication-tables.js
@@ -1,92 +1,14 @@
-// Tafel van 1
-console.log("Tafel van 1:");
-var t = 1;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
-} 
-// Tafel van 2
-console.log("Tafel van 2:");
-var t = 2;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
-} 
-// Tafel van 3
-console.log("Tafel van 3:");
-var t = 3;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
-} 
-// Tafel van 4
-console.log("Tafel van 4:");
-var t = 4;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
-} 
-// Tafel van 5
-console.log("Tafel van 5:");
-var t = 5;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
-} 
-// Tafel van 6
-console.log("Tafel van 6:");
-var t = 6;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
-} 
-// Tafel van 7
-console.log("Tafel van 7:");
-var t = 7;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
-} 
-// Tafel van 8
-console.log("Tafel van 8:");
-var t = 8;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
-} 
-
-// Tafel van 9
-console.log("Tafel van 9:");
-var t = 9;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
+// Multiplication tables
+const multiplier = 9;
+for (let i = 0; i <= 10; i++) {
+  let result = multiplier * i;
+  console.log(multiplier + " x " + i + " = " + result);
 }
 
-// Tafel van 10
-console.log("Tafel van 10:");
-var t = 10;
-var i = 1;
-
-while (i <= 10) {
-    console.log(t + " X " + i + " = " + i * t);
-    i++;
-} 
+// Bonus
+for (let multiplier = 0; multiplier <= 10; multiplier++) {
+  for (let i = 0; i <= 10; i++) {
+    let result = multiplier * i;
+    console.log(multiplier + " x " + i + " = " + result);
+  }
+}
